42ebb7393c0e1e9d4813c11839a4b2b35a40b032
Megabyte-webdev <afolabimubarak18@gmail.com>
2026-02-19 12:18:19 +0100

    new update

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import dotenv from "dotenv";
 dotenv.config();
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.POSTGRES_DATABASE_URL,
 });
 
 // Test connection and log status
